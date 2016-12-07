671fdb8d38f368e44488170448f48ac1626fa3b5
JW-Vinayak <vinayak.mote@Xoriant.com>
2016-12-07 11:47:02 +0530

    Separated web and server code

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
+app.get('/', function(req, response) {
+    fs.readFile('./index.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
+    })
+})
+
+
+app.use(express.static('web'));
+
+app.listen(8000, function() {
+    console.log('Server ready and listening on port : 8000');
+})
+
+
+
+
+
+
+
+
+
+/*       
+    http.createServer(function(request, response) {  
+
     }).listen(8000);
-});
+});*/
