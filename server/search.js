95a5bff7cafbc88c759e4278d31117be14270da4
JW-Vinayak <vinayak.mote@Xoriant.com>
2016-12-15 19:36:01 +0530

    search on local machine, not tested yet.

--- server/search.js
+++ server/search.js
@@ -3,7 +3,9 @@ var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
 var shell = require('shelljs');
 var open = require("open");
-var path = require('path')
+var path = require('path');
+var glob = require('glob');
+var fs = require('fs');
 
 module.exports = function(req, res) {
     var words = req.body.script.split(" "), keywords, where;
@@ -31,7 +33,13 @@ module.exports = function(req, res) {
     } else  if(where === 'youtube') {
         open('http://www.youtube.com/results?search_query=' + keywords.split(" ").join("+"));
         return res.json({'msg': 'youtube search with keywords "' + keywords + '" is open now'});
-    } else {
+    }  /*else  if(where === 'my machine') {
+        where = where.replace('file','');
+        var files = glob.sync(where, {'cwd': 'D:\\'})
+        console.log('files are', files);
+        exec('start "" "'+files[0]+'"');
+        return res.json({'msg': 'youtube search with keywords "' + keywords + '" is open now'});
+    }*/ else {
         return res.json({'msg': 'I did not understand your command'});
     }
 
